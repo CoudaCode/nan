755b6972b76be6b7210aad8cf86fa4589ac30bc7
Mariamwanda <mariamwandaogo7@gmail.com>
2023-11-02 17:08:47 +0000

    page contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,17 +1,353 @@
-
-import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Topbar from "../../components/Topbar/Topbar";
-import "./Contact.css"
+import "./Contact.css";
+import {
+  FaSearch,
+  FaPlus,
+  FaChevronLeft,
+  FaChevronRight,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 function Contact() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Topbar />
-        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">
-          contact
+        <div className="h-full overflow-y-auto p-4 bg-[#1E2029] Contact">
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div className="flex items-center justify-between pb-4">
+              <div className="relative">
+                <input
+                  type="text"
+                  id="search"
+                  className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 focus:outline-none  rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="recherche"
+                />
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <FaSearch style={{ color: "#6870e0 " }} />
+                </div>
+              </div>
+              <div>
+                <button
+                  id="btn"
+                  className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none  focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button"
+                >
+                  <FaPlus style={{ margin: "8px " }} />
+                  contact d’mportation
+                </button>
+                <button
+                  id="btn"
+                  className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none  focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button"
+                >
+                  <FaPlus style={{ margin: "8px " }} />
+                  ajouter le contact
+                </button>
+              </div>
+            </div>
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sl text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr className="titre">
+                    <th scope="col" className="p-4">
+                      <div className="flex items-center"></div>
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Nom & Prenom
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Email
+                    </th>
+                    <th scope="col" className="px-3 py-3">
+                      Numéro Whatsapp
+                    </th>
+                    <th scope="col" className="px-4 py-3">
+                      Pays
+                    </th>
+                    <th scope="col" className="px-2 py-1">
+                      Langue
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-1"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250121822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-2"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-4">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-4">+2250141822918</td>
+                    <td className="px-1 py-4">Côte d’ivoire</td>
+                    <td className="px-1 py-4">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250141822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-3"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Abraham konan
+                    </th>
+                    <td className="px-1 py-2">abrahamkonany@gmail.com </td>
+                    <td className="px-1 py-2">+2250121822918</td>
+                    <td className="px-1 py-2">Côte d’ivoire</td>
+                    <td className="px-1 py-2">Français</td>
+                  </tr>
+                </tbody>
+              </table>
+              <nav
+                className="flex items-center justify-between pt-4"
+                aria-label="Table navigation"
+              >
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold text-gray-900 dark:text-white"></span>{" "}
+                  <span className="font-semibold text-gray-900 dark:text-white"></span>
+                </span>
+                <ul className="inline-flex -space-x-px text-sm h-8">
+                  <li>
+                    <Link
+                      to="#"
+                      className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      <FaChevronLeft />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="#"
+                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      2
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="#"
+                      className="flex items-center justify-center px-3 h-8  border border-gray-300  border-gray-700  red:text-white"
+                      style={{ background: "#6870e0" }}
+                    >
+                      3
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      4
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="#"
+                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    >
+                      <FaChevronRight />
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,14 +1,39 @@
-import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Topbar from "../../components/Topbar/Topbar";
-import "./Reports.css"
+import "./Reports.css";
+import { FaSearch, FaPlus } from "react-icons/fa";
 function reports() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Topbar />
-        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">Reports</div>
+        <div className="h-full overflow-y-auto p-4 bg-[#1E2029] Reports">
+          {" "}
+          <div className="flex items-center justify-between pb-4">
+            <div className="relative">
+              <input
+                type="text"
+                id="table-search"
+                className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 focus:outline-none  rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="recherche"
+              />
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <FaSearch style={{ color: "#6870e0 " }} />
+              </div>
+            </div>
+            <div>
+              <button
+                id="btn"
+                className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none  focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                type="button"
+              >
+                <FaPlus style={{ margin: "8px " }} />
+                Restaurer
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
