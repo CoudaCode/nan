b84bd404c33e33294292ec1a4b9837805b903f01
Diarassouba Mamadou <couda.dm@gmail.com>
2023-11-14 16:40:44 +0000

    pk

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -53,44 +53,14 @@ function Sidebar() {
   return (
     <>
       <div className="Sidebar bg-[#1E2029] sm:w-60 min-h-screen w-14 pt-4 transition-all">
-        {/* 
-        <div className="text-center text-white p-4 sm:p-6">
-          <span className="text-violet-800 font-bold text-2xl sm:text-3xl">
-            NaN-
-          </span>
-          <span className="text-violet-800 font-bold text-2xl sm:text-3xl">
-            Send
-          </span>
-        </div>
-        <ul className="mt-11 bg-white rounded-lg">
-          {menuItems.map((item, index) => (
-            <li
-              key={index}
-              className={`${
-                path === item.to ? "active" : ""
-              } hover:bg-gray-800 cursor-pointer text-gray-800 sm:justify-start px-4 h-12 flex items-center justify-center`}
-              onClick={() => handleItemClick(index)}>
-              {item.icon}
-              <Link to={item.to}>
-                <span className="ml-3 hidden sm:block text-gray-800 font-semibold tracking-wide hover:text-white transition-colors">
-                  {item.text}
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="dropdown rounded-4 m-2 mt-20 p-4 bg-white rounded-2xl">
-          <h5 className="text-black font-bold">NAN DIGITAL ACADEMY</h5>
-          <h6 className="font-bold text-gray-500 text-center">Version: 1.0.0.11</h6>
-        </div>
-      */}
-        {/* <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}> */}
-        <div className={`sidebar open `}>
+
+        <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        {/* <div className={`sidebar open `}> */}
           <div className="logo-details">
             <i className="bx bxl-c-plus-plus icon"></i>
             <div className="logo_name">NaN-Send</div>
-            {/* <i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i> */}
-            <i className="bx bx-menu" id="btn"></i>
+            <i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i>
+            {/* <i className="bx bx-menu" id="btn"></i> */}
           </div>
           <ul className="nav-list">
             <li>
