5024c2acb9558d3635e5d0f8c094f8396c92eca6
Wandaogo Mariam <mariam.wandaogo@uvci.edu.ci>
2023-11-16 17:35:17 +0000

    ok

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,20 +1,14 @@
-
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
-// import Topbar from "../../components/Topbar/Topbar";
-import "./Contact.css"
+import "./Contact.css";
 function Contact() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-       
-        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">
-          contact
-          </div>
+        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">contact</div>
       </div>
     </div>
   );
 }
-
 export default Contact;
