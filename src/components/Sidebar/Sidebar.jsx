bb051e4f342affe71992e427d8bdb7acbfff027b
Diarassouba Mamadou <couda.dm@gmail.com>
2023-11-19 23:55:42 +0000

    ok

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -53,14 +53,11 @@ function Sidebar() {
   return (
     <>
       <div className="Sidebar bg-[#1E2029] sm:w-60 min-h-screen w-14 pt-4 transition-all">
-
         <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
-        {/* <div className={`sidebar open `}> */}
           <div className="logo-details">
             <i className="bx bxl-c-plus-plus icon"></i>
             <div className="logo_name">NaN-Send</div>
             <i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i>
-            {/* <i className="bx bx-menu" id="btn"></i> */}
           </div>
           <ul className="nav-list">
             <li>
@@ -80,6 +77,13 @@ function Sidebar() {
                 <span className="tooltip">{menuItem.text}</span>
               </li>
             ))}
+
+            {/* <li className="card">
+              <div className="card-body">
+                <h2 className="card-title">Nan Digital Academy</h2>
+                <p className="card-text">Version 1.0.0</p>
+              </div>
+            </li> */}
           </ul>
         </div>
       </div>
