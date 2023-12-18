923faefdb9905b83cb8c310496395d02fae06137
CoudaCode <couda.dm@gmail.com>
2023-12-18 22:09:35 +0000

    ok

--- src/pages/Broadcast/Broadcast.jsx
+++ src/pages/Broadcast/Broadcast.jsx
@@ -1,18 +1,13 @@
-
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 // import Topbar from "../../components/Topbar/Topbar";
-import "./Broadcast.css"
+import "./Broadcast.css";
 function Broadcast() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-        {/*<Topbar />*/}
-        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">
-          Broadcast
-          
-           </div>
+        <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">Broadcast</div>
       </div>
     </div>
   );
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,82 +1,71 @@
-
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 // import Topbar from "../../components/Topbar/Topbar";
-import "./Profile.css"
+import "./Profile.css";
 
-import img from "../../assets/images/img-profile.jpg"
-import poinVert from "../../assets/images/pointVerts.png"
+import img from "../../assets/images/img-profile.jpg";
+import poinVert from "../../assets/images/pointVerts.png";
 function Profile() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-        {/*<Topbar />*/}
         <div className="h-full overflow-y-auto p-4 bg-[#1E2029]">
-        Profile
+          Profile
+          <div className="profile">
+            <div className="cardsPofit">
+              <div className="profileEspace">
+                <div className="profilImage">
+                  <img src={img} alt="" />
+                </div>
+                <div className="profilOffline">
+                  <div className="pointVer">
+                    <img src={poinVert} alt="" />
+                  </div>
+                  <p>En Ligne</p>
+                </div>
+              </div>
+              <div className="profileTitle">
+                <div className="proTitre">
+                  <h2>Informations Utilisateurs</h2>
+                </div>
+                <div className="profilInfos">
+                  <div className="profilDetail">
+                    <h4>Nom:</h4>
+                    <input type="text" placeholder="KOUAME" />
+                  </div>
+                  <br />
+                  <div className="profilDetail">
+                    <h4>Prenoms:</h4>
+                    <input
+                      type="text"
+                      placeholder="KOUAME KONAN FRANCK OLIVIER"
+                    />
+                  </div>
 
-
-
-<div className="profile">
-
-
-  <div className="cardsPofit">
-<div className="profileEspace">
-<div className="profilImage">
-<img src={img} alt="" />
-</div>
-<div className="profilOffline">
-<div className="pointVer">
-  <img src={poinVert} alt="" />
-</div>
-<p>En Ligne</p>
-</div>
-</div>
-<div className="profileTitle">
-  <div className="proTitre">
-  <h2>Informations Utilisateurs</h2>
-  </div>
-  <div className="profilInfos">
-
-    <div className="profilDetail">
-      <h4>Nom:</h4>
-      <input type="text" placeholder="KOUAME"/>
-    </div>
-<br />
-    <div className="profilDetail">
-      <h4>Prenoms:</h4>
-      <input type="text" placeholder="KOUAME KONAN FRANCK OLIVIER"/>
-    </div>
-
-<br />
-    <div className="profilDetail">
-      <h4>Telephone:</h4>
-      <input type="text" placeholder="0789105791"/>
-    </div>
-<br />
-    <div className="profilDetail">
-      <h4>E-mail:</h4>
-      <input type="text" placeholder="kouamefranckolivier45@gmail.com"/>
-    </div>
-
-
-
-  </div>
-  <div className="modification">
-    <button className="myButtom">Modifier Profile</button>
-  </div>
-</div>
-
-  </div>
-
-</div>
-
-
-
+                  <br />
+                  <div className="profilDetail">
+                    <h4>Telephone:</h4>
+                    <input type="text" placeholder="0789105791" />
+                  </div>
+                  <br />
+                  <div className="profilDetail">
+                    <h4>E-mail:</h4>
+                    <input
+                      type="text"
+                      placeholder="kouamefranckolivier45@gmail.com"
+                    />
+                  </div>
+                </div>
+                <div className="modification">
+                  <button className="myButtom">Modifier Profile</button>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     </div>
-    
   );
 }
 
