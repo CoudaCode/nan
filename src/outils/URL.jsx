e32cd336bfba62a868b7dc9ff335c12ad7e39238
Diarassouba Mamadou <119653418+CoudaCode@users.noreply.github.com>
2023-12-20 08:31:13 +0000

    Update URL.jsx

--- src/outils/URL.jsx
+++ src/outils/URL.jsx
@@ -1,6 +1,6 @@
 // const ApiUrl = "https://nan-send-api.onrender.com/api/";
-const ApiUrl = "http://localhost:3000/api/";
+const ApiUrl = "https://nan-send-api.onrender.com/api/";
 
-const FrontUrl = "http://localhost:5173/";
+const FrontUrl = "https://nan-send.vercel.app/";
 
 export {ApiUrl, FrontUrl};
