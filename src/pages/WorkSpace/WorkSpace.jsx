f8b7d952194bc5d86a4051ab19b4dc1a665141e6
Diarassouba Mamadou <couda.dm@gmail.com>
2024-01-18 10:30:59 +0100

    ajouter de formulaire de mise

--- src/pages/WorkSpace/WorkSpace.jsx
+++ src/pages/WorkSpace/WorkSpace.jsx
@@ -3,73 +3,8 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 // import Topbar from "../../components/Topbar/Topbar";
 import "./WorkSpace.css";
 import PropTypes from 'prop-types';
+import CreateContactModal  from "./../WorkSpace/workspaceModal"
 
-const CreateContactModal = ({ isOpen, onClose }) => {
-  if (!isOpen) {
-    return null;
-  }
-
-  const [contactData, setContactData] = useState({
-    nom: "",
-    prenom: "",
-    email: "",
-    telephone: "",
-    whatsapp: "",
-  });
-
-  const handleCreateContact = () => {
-    // Handle the create contact logic here
-    // Use the 'contactData' state for the contact details
-    // ...
-
-    // Clear the form and close the modal
-    setContactData({
-      nom: "",
-      prenom: "",
-      email: "",
-      telephone: "",
-      whatsapp: "",
-    });
-
-    onClose();
-  };
-
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      {/* Overlay */}
-      <div className="bg-gray-800 bg-opacity-75 absolute inset-0"></div>
-
-      {/* Modal */}
-      <div className="mx-auto w-2/4 bg-gray-900 max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 rounded-lg shadow-2xl z-10">
-        <div className="mx-auto max-w-lg text-center">
-          <h1 className="text-2xl text- font-bold text-white sm:text-3xl">
-            Remplissez les champs svp!
-          </h1>
-        </div>
-
-        <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-          <p>En cours ....</p>
-
-          <div className="mt-4 flex gap-2">
-            <button
-              type="button"
-              className="rounded bg-violet-500  hover:bg-violet-900  px-4 py-2 text-sm font-medium text-white"
-              onClick={handleCreateContact}>
-              Confirmer l&apos;ajout
-            </button>
-
-            <button
-              type="button"
-              className="rounded bg-red-500 hover:bg-red-900  px-4 py-2 text-sm font-medium text-white"
-              onClick={onClose}>
-              Annuler
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
 function WorkSpace() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const toggleSidebar = () => {
