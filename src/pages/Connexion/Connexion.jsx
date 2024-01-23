9172dda16d14984d4c0e710bd898661253dd4fc3
Diarassouba Mamadou <couda.dm@gmail.com>
2024-01-23 13:56:55 +0000

    ok

--- src/pages/Connexion/Connexion.jsx
+++ src/pages/Connexion/Connexion.jsx
@@ -1,102 +1,137 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
-import { toast } from "react-toastify";
-import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
 import Cookie from "js-cookie";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
-import "./Connexion.css";
+import { toast } from "react-toastify";
 import { ApiUrl } from "../../outils/URL";
+import "./Connexion.css";
+import { FaArrowLeft } from "react-icons/fa";
+function Connexion() {
+  const navigate = useNavigate();
+  const login = async (data) => {
+    const result = await axios.post(ApiUrl + "auth/login", data);
+    return result;
+  };
 
-  function Connexion() {
-    const navigate = useNavigate();
-    const login = async (data) => {
-      const result = await axios.post( ApiUrl+ "auth/login", data );
-      return result;
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ email: "", password: "" });
 
-    const { register, handleSubmit, formState: { errors } } = useForm({ email: "", password: "" });
+  const { mutate: loginUser } = useMutation({
+    mutationFn: (Mydata) => login(Mydata),
+    onSuccess: (succes) => {
+      console.log("demo", succes.data);
+      toast.success(succes.data.message);
+      Cookie.set("NaN_Digit_Sender_Token_Secretly", succes.data.token, {
+        expires: 1,
+      });
+      setTimeout(() => {
+        sessionStorage.setItem(
+          "NaN_Digit_Sender_Token_Secretly",
+          JSON.stringify(succes.data.id)
+        );
+        navigate("/dashboard");
+      }, 3000);
+    },
+    onError: (e) => {
+      console.log(e.response.data.message);
+      toast.error(e.response.data.message);
+    },
+  });
 
-    const { mutate: loginUser } = useMutation({
-      mutationFn: (Mydata) => login(Mydata),
-      onSuccess: (succes) => {
-        console.log("demo", succes.data);
-        toast.success(succes.data.message);
-        Cookie.set("NaN_Digit_Sender_Token_Secretly", succes.data.token, { expires: 1 });
-        setTimeout(() => {
-          sessionStorage.setItem("NaN_Digit_Sender_Token_Secretly", JSON.stringify(succes.data.id));
-          navigate("/dashboard");
-        }, 3000);
-      },
-      onError: (e) => {
-        console.log(e.response.data.message);
-        toast.error(e.response.data.message);
-      },
-    });
+  const onSubmit = (data) => loginUser(data);
+  return (
+    <>
+      <div className="Connexion">
+        <div className="container">
+          <Link to="/">
+            <button className="back-button">
+              <FaArrowLeft /> Retour à l'accueil
+            </button>
+          </Link>
+          <div className="heading mt-9">Connexion</div>
+          <form action="" className="form" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              {...register("email", {
+                require: true,
+                minLength: 5,
+                maxLength: 50,
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                  message: "Veuillez entrer une adresse mail valide.",
+                },
+                validate: {
+                  notEmpty: (value) =>
+                    /^\S+$/.test(value) ||
+                    "Ce champ ne peut pas être vide ou contenir uniquement des espaces.",
+                },
+              })}
+              className="input"
+              type="email"
+              id="email"
+              placeholder="Adresse Email"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+            <input
+              required=""
+              className="input"
+              type="password"
+              id="password"
+              placeholder="Mot de Passe"
+              {...register("password", {
+                require: true,
+                minLength: 8,
+                maxLength: 50,
+                pattern: {
+                  value:
+                    /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$/,
+                  message:
+                    "Le mot de passe doit contenu au moins: 8 caractères, une majuscule, une minuscule, un symbole et aucun espace.",
+                },
+                validate: {
+                  notEmpty: (value) =>
+                    /^\S+$/.test(value) ||
+                    "Ce champ ne peut pas être vide ou contenir uniquement des espaces.",
+                },
+              })}
+            />
 
-    const onSubmit = (data) => loginUser(data);
-    return (
-      <> 
-        <div className="Connexion">
-          <div className="container">
-            <div className="heading">Connexion</div>
-            <form action="" className="form" onSubmit={handleSubmit(onSubmit)}>
-              <input
-                {...register("email", {
-                  require: true,
-                  minLength: 5,
-                  maxLength: 50,
-                  pattern: { value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i, message: "Veuillez entrer une adresse mail valide." },
-                  validate: {notEmpty: value => /^\S+$/.test(value) || "Ce champ ne peut pas être vide ou contenir uniquement des espaces."}
-                })}
-                className="input"
-                type="email"
-                id="email"
-                placeholder="Adresse Email"
-              />
-              {errors.email && (<p className="text-red-500 text-sm">{errors.email.message}</p>)}
-              <input
-                required=""
-                className="input"
-                type="password"
-                id="password"
-                placeholder="Mot de Passe"
-                {...register("password", {
-                  require: true,
-                  minLength: 8,
-                  maxLength: 50,
-                  pattern: {
-                    value: /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,}$/,
-                    message: "Le mot de passe doit contenu au moins: 8 caractères, une majuscule, une minuscule, un symbole et aucun espace."
-                  },
-                  validate: {notEmpty: value => /^\S+$/.test(value) || "Ce champ ne peut pas être vide ou contenir uniquement des espaces."}
-                })}
-              />
-              {errors.password && (<p className="text-red-500 text-sm">{errors.password.message}</p>)}
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
 
+            <div className="text-red-900 mt-11 text-sm text-center">
+              <div>J'ai pas de compte ? </div>
               <span className="forgot-password">
-                <a href="#">Mot de passe oublié ?</a>
+                <Link to="/inscription">S'inscrire</Link>
               </span>
-              <input
-                className="login-button"
-                type="submit"
-                value="Se connecter"
-              />
-            </form>
-            <div className="social-account-container">
-              <span className="title">Ou connectez-vous avec</span>
-              <div className="social-accounts">
-                <Link>
-                  <button className="social-button google">
-                    <span>{<FcGoogle />}</span>
-                  </button>
-                </Link>
-              </div>
+            </div>
+            <input
+              className="login-button"
+              type="submit"
+              value="Se connecter"
+            />
+          </form>
+          <div className="social-account-container">
+            <span className="title">Ou connectez-vous avec</span>
+            <div className="social-accounts">
+              <Link>
+                <button className="social-button google">
+                  <span>{<FcGoogle />}</span>
+                </button>
+              </Link>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
 
-  export default Connexion;
+export default Connexion;
