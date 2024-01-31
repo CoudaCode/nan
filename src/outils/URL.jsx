d83fe7926c706f655e3ba198dda41ebd50e97f47
nfcdjobo <nfcdjobo@gmail.com>
2024-01-31 01:52:41 +0000

    Last commit

--- src/outils/URL.jsx
+++ src/outils/URL.jsx
@@ -1,9 +1,12 @@
-// const ApiUrl = "https://nan-send-api.onrender.com/api/";
-const ApiUrl = "http://localhost:3000/api/";
-// const ApiUrl = "https://nan-send.onrender.com/api/";
 
-// const ApiUrl = "https://nansendmessage.onrender.com/api/";
+// const ApiUrl = "http://localhost:3000/api/";
 
-const FrontUrl = "http://localhost:5173/";
+// const FrontUrl = "http://localhost:5173/";
+
+
+
+const ApiUrl = "https://nansendmessage.onrender.com/api/";
+
+const FrontUrl = "https://newnansend.onrender.com/";
 
 export {ApiUrl, FrontUrl};
