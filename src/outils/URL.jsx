24d34100b49c65b0f507efa5deee04d7bc27a2a2
Diarassouba Mamadou <couda.dm@gmail.com>
2024-02-01 10:50:42 +0000

    mise à jour du lien api

--- src/outils/URL.jsx
+++ src/outils/URL.jsx
@@ -1,6 +1,6 @@
 // const ApiUrl = "https://nan-send-api.onrender.com/api/";
-const ApiUrl = "https://nansendmessage.onrender.com/api/";
-// const ApiUrl = "https://nan-send.onrender.com/api/";
+// const ApiUrl = "https://nansendmessage.onrender.com/api/";
+const ApiUrl = "https://nan-send-api.onrender.com/api/";
 
 // const ApiUrl = "https://nansendmessage.onrender.com/api/";
 
