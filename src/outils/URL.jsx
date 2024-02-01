52e9a3b750d1b29de0ff06942b4878880fe62178
Diarassouba Mamadou <couda.dm@gmail.com>
2024-02-01 10:00:07 +0000

    ok

--- src/outils/URL.jsx
+++ src/outils/URL.jsx
@@ -1,9 +1,9 @@
- // const ApiUrl = "https://nan-send-api.onrender.com/api/";
-const ApiUrl = "http://localhost:3000/api/";
+// const ApiUrl = "https://nan-send-api.onrender.com/api/";
+const ApiUrl = "https://nansendmessage.onrender.com/api/";
 // const ApiUrl = "https://nan-send.onrender.com/api/";
 
 // const ApiUrl = "https://nansendmessage.onrender.com/api/";
 
-const FrontUrl = "http://localhost:5173/";
+const FrontUrl = "https://nan-send.vercel.app";
 
-export {ApiUrl, FrontUrl};
+export { ApiUrl, FrontUrl };
